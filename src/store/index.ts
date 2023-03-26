ae126f9c4ac39f6ab007d4a0d1f1c3cee75dd04a
victorraji <vheektoure@gmail.com>
2023-03-26 16:48:46 +0100

    update fioxes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import type Entry from "@/types/Entry";
 
+export type State = { todos: Entry[] };
+
+const state: State = { todos: [] };
+
 export const store = createStore({
-  state: {
-    todos: [],
-  },
+  state,
   mutations: {
     ADD_TODO(state, payload: Entry) {
       state.todos.unshift(payload);
